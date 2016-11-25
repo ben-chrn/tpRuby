bed1fb9294ae1ec5fbde37e04bc2cce98ebb1bba
stephen-richard <stephen.richard@hetic.net>
2016-11-25 14:10:00 +0100

    20/20 easy

--- tp3/app/controllers/posts_controller.rb
+++ tp3/app/controllers/posts_controller.rb
@@ -10,8 +10,34 @@ class PostsController < ApplicationController
   end
 
   def show
+  	@post = Post.find_by_id(params[:id]) or not_found
+  end
+
+  def not_found
+  	render :status => 404
+  end
+
+  def create 
+  	@post = Post.new(post_params)
+
+  	if @post.save
+  		redirect_to posts_path
+	else
+		flash[:error] = "User can't be blank"
+		render action: "new"
+	end
+  end
+
+  def post_params
+  	params.require(:post).permit(:user_id, :message)
   end
 
   def edit
+  	@post = Post.find_by_id(params[:id])
   end
+
+  def update
+  	@post = Post.find_by_id(params[:id]).update_attributes(:message => params[:post][:message])
+  end
+
 end
